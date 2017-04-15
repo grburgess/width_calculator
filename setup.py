41577dd588dca32d45e50af03c4342cc32b2706b
grburgess <jmichaelburgess@gmail.com>
2017-04-16 01:13:28 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='width_calculator',
+    version='0.1',
+    description='calculate the spectral width of 3ML models',
+    author='J. Michael Burgess',
+    author_email='jmichaelburgess@gmail.com',
+    packages=['width_calculator'], )
